098b229adddac135d3ed1af36655e9330b3bd2b7
Vladyslav <bravilmaksim@gmail.com>
2026-08-25 13:37:43 +0300

    (Fixed code for Hand module)

--- Right_Shoulder_2.0/ESP32C3_HAND_2.0/include/ArmPackets.h
+++ Right_Shoulder_2.0/ESP32C3_HAND_2.0/include/ArmPackets.h
@@ -1,24 +1,20 @@
-#pragma once
+#ifndef ARM_PACKETS_H
+#define ARM_PACKETS_H
+
 #include <Arduino.h>
 
-struct __attribute__((packed)) SensorData {
-  float yaw;
-  float pitch;
-  float roll;
-  uint8_t calibSys;
-  uint8_t calibGyro;
+#pragma pack(push, 1)
+struct SensorData {
+    float yaw;
+    float pitch;
+    float roll;
 };
 
-struct __attribute__((packed)) FullArmPacket {
-  uint8_t header = 0xAA;
-  SensorData hand;       
-  SensorData forearm;    
-  SensorData shoulder;  
-  uint8_t checksum;
+struct FullArmPacket {
+    SensorData hand;
+    SensorData forearm;
+    SensorData shoulder;
 };
+#pragma pack(pop)
 
-inline uint8_t calculateChecksum(const uint8_t* data, size_t len) {
-  uint8_t sum = 0;
-  for (size_t i = 0; i < len; i++) sum ^= data[i];
-  return sum;
-}
+#endif
--- Right_Shoulder_2.0/ESP32C3_HAND_2.0/src/main.cpp
+++ Right_Shoulder_2.0/ESP32C3_HAND_2.0/src/main.cpp
@@ -6,37 +6,51 @@
 
 #define SDA_PIN 3
 #define SCL_PIN 2
-#define TX_PIN  4  // Передача на Передпліччя (GPIO 4 -> GPIO 1 Передпліччя)
+#define TX_PIN  4
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
 HardwareSerial LinkSerial(1);
-
 SensorData handData;
+bool bnoReady = false;
 
 void setup() {
   LinkSerial.begin(115200, SERIAL_8N1, -1, TX_PIN);
+
+  // 1. Примусове вибивання I2C з заторів (Bus Recovery)
+  pinMode(SCL_PIN, OUTPUT);
+  pinMode(SDA_PIN, INPUT_PULLUP);
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(SCL_PIN, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(SCL_PIN, LOW);
+    delayMicroseconds(5);
+  }
+
   Wire.begin(SDA_PIN, SCL_PIN, 100000);
+  Wire.setTimeOut(50);
 
   if (bno.begin(OPERATION_MODE_NDOF)) {
     bno.setExtCrystalUse(false);
+    bnoReady = true;
   }
 }
 
 void loop() {
-  sensors_event_t event;
-  bno.getEvent(&event);
+  // 2. Якщо при старті BNO055 не прокинувся — пробуємо підключити в циклі
+  if (!bnoReady) {
+    if (bno.begin(OPERATION_MODE_NDOF)) {
+      bno.setExtCrystalUse(false);
+      bnoReady = true;
+    }
+  } else {
+    sensors_event_t event;
+    bno.getEvent(&event);
+    handData.yaw = event.orientation.x;
+    handData.pitch = event.orientation.y;
+    handData.roll = event.orientation.z;
+  }
 
-  uint8_t sys, gyro, accel, mag;
-  bno.getCalibration(&sys, &gyro, &accel, &mag);
-
-  handData.yaw = event.orientation.x;
-  handData.pitch = event.orientation.y;
-  handData.roll = event.orientation.z;
-  handData.calibSys = sys;
-  handData.calibGyro = gyro;
-
-  // Шлемо 14 байт
+  // Відправляємо дані на Передпліччя
   LinkSerial.write((uint8_t*)&handData, sizeof(SensorData));
-
   delay(20);
 }
