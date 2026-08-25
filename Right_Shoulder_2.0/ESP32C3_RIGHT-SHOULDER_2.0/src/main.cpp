b357ee1d2de1c5461515f3e416abb9030c3ac46f
Vladyslav <bravilmaksim@gmail.com>
2026-08-25 13:38:23 +0300

    (Fixed code for RightShoulder module)

--- Right_Shoulder_2.0/ESP32C3_RIGHT-SHOULDER_2.0/src/main.cpp
+++ Right_Shoulder_2.0/ESP32C3_RIGHT-SHOULDER_2.0/src/main.cpp
@@ -6,49 +6,54 @@
 
 #define SDA_PIN 3
 #define SCL_PIN 2
-#define RX_PIN  1  // Прийом від Передпліччя
+#define RX_PIN  1
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
 HardwareSerial LinkSerial(1);
-
 FullArmPacket fullArm;
 
 void setup() {
   Serial.begin(115200);
-
-  uint32_t startWait = millis();
-  while (!Serial && (millis() - startWait < 3000)) delay(10);
-
   LinkSerial.begin(115200, SERIAL_8N1, RX_PIN, -1);
-  Wire.begin(SDA_PIN, SCL_PIN, 100000);
+  // Додати у setup() Плеча та Передпліччя ПЕРЕД Wire.begin()
+pinMode(SDA_PIN, INPUT_PULLUP);
+pinMode(SCL_PIN, OUTPUT);
 
+for (int i = 0; i < 10; i++) {
+  digitalWrite(SCL_PIN, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(SCL_PIN, LOW);
+  delayMicroseconds(5);
+}
+
+
+  Wire.begin(SDA_PIN, SCL_PIN, 100000);
   if (bno.begin(OPERATION_MODE_NDOF)) {
     bno.setExtCrystalUse(false);
   }
 }
 
 void loop() {
-  // 1. Приймаємо 28 байт від каскаду (Кисть + Передпліччя)
-  if (LinkSerial.available() >= (sizeof(SensorData) * 2)) {
+  // Зчитуємо ровно 24 байти (Hand + Forearm)
+  while (LinkSerial.available() >= (sizeof(SensorData) * 2)) {
     LinkSerial.readBytes((uint8_t*)&fullArm.hand, sizeof(SensorData));
     LinkSerial.readBytes((uint8_t*)&fullArm.forearm, sizeof(SensorData));
   }
 
-  // 2. Зчитуємо власний BNO055
+  // Власний BNO055 Плеча
   sensors_event_t event;
   bno.getEvent(&event);
-  uint8_t sys, gyro, accel, mag;
-  bno.getCalibration(&sys, &gyro, &accel, &mag);
-
   fullArm.shoulder.yaw = event.orientation.x;
   fullArm.shoulder.pitch = event.orientation.y;
   fullArm.shoulder.roll = event.orientation.z;
 
-  // 3. Відправляємо JSON у ПК
-  Serial.printf("{\"shoulder\":{\"p\":%.1f,\"y\":%.1f,\"r\":%.1f},\"forearm\":{\"p\":%.1f,\"y\":%.1f,\"r\":%.1f},\"hand\":{\"p\":%.1f,\"y\":%.1f,\"r\":%.1f}}\n",
-                fullArm.shoulder.pitch, fullArm.shoulder.yaw, fullArm.shoulder.roll,
-                fullArm.forearm.pitch, fullArm.forearm.yaw, fullArm.forearm.roll,
-                fullArm.hand.pitch, fullArm.hand.yaw, fullArm.hand.roll);
+  // Фільтр від сміття
+  if (abs(fullArm.hand.roll) < 361.0 && abs(fullArm.forearm.roll) < 361.0) {
+    Serial.printf("{\"shoulder\":{\"p\":%.1f,\"y\":%.1f,\"r\":%.1f},\"forearm\":{\"p\":%.1f,\"y\":%.1f,\"r\":%.1f},\"hand\":{\"p\":%.1f,\"y\":%.1f,\"r\":%.1f}}\n",
+                  fullArm.shoulder.pitch, fullArm.shoulder.yaw, fullArm.shoulder.roll,
+                  fullArm.forearm.pitch, fullArm.forearm.yaw, fullArm.forearm.roll,
+                  fullArm.hand.pitch, fullArm.hand.yaw, fullArm.hand.roll);
+  }
 
   delay(20);
 }
